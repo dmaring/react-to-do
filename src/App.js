7478eeff2aab92681be9ec149212b77135bf1444
Drew Maring <andrew.maring@gmail.com>
2018-09-23 17:32:07 -0500

    complete events checkpoint

--- src/App.js
+++ src/App.js
@@ -10,23 +10,46 @@ class App extends Component {
         { description: "Walk the cat", isCompleted: true },
         { description: "Throw the dishes away", isCompleted: false },
         { description: "Buy new dishes", isCompleted: false },
-      ]
-    }
+      ],
+      newTodoDescription: ''
+    };
+  }
+
+  toggleComplete(index) {
+    // create a copy of the todos list to modify
+    const todos = this.state.todos.slice();
+    // grab the todo corresponding to index
+    const todo = todos[index];
+    // if todo is true -> false, false -> true
+    todo.isCompleted = todo.isCompleted ? false : true;
+    // set the state of the todos with the new todos list
+    this.setState({ todos: todos })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.newTodoDescription) { return }
+    const newTodo = { description: this.state.newTodoDescription, isCompleted: false };
+    // pass a new todos list a new array along with the new ToDo
+    this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' });
+  }
+
+  handleChange(e) {
+    this.setState({ newTodoDescription: e.target.value })
   }
 
   render() {
     return (
       <div className="App">
         <ul>
-
-          <ToDo />
-          <ToDo />
-
           { this.state.todos.map( (todo, index) =>
-            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
           )}
-
         </ul>
+        <form onSubmit={ (e) => this.handleSubmit(e) }>
+          <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
